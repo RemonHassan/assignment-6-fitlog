36e1f8f0fbc706481cff974cc1d2e4c7dde8e084
RemonHassan <fmremonhassan203@gmail.com>
2026-09-24 22:49:23 +0600

    added the navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <h2>HomePage...</h2>;
+  return <h2 className="text-white font-bold">HomePage...</h2>;
 }
---
+++ src/app/plans/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MyPlanPage = () => {
+  return (
+    <div>
+      <h2 className="text-white font-bold">My Plans</h2>
+    </div>
+  );
+};
+
+export default MyPlanPage;
